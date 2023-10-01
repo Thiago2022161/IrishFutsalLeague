4f0fb8614f75049f1ab297c0ab5652accb32b924
Thiago2022161 <2022161@student.cct.ie>
2023-10-01 01:16:13 +0100

    Initial commit

---
+++ src/irishfutsalleague/IrishFutsalLeague.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package irishfutsalleague;
+
+/**
+ *
+ * @author thiagogoncos
+ */
+public class IrishFutsalLeague {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("AUL Futsal Premier Division 23/24 clubs:");
+        System.out.println("1. Blue Magic FC");
+        System.out.println("2. Leprechaun FC");
+        System.out.println("3. Pro Brazil FC");
+        System.out.println("4. Clontarf Vikings FC");
+        System.out.println("5. St Itas FC");
+        System.out.println("6. Croata Dublin FC");
+    }
+    
+}
